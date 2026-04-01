acf3c6e9e0e97b5a629e7ecb1814f9f9e74a88a2
Ishan3131 <chetwanikrish@gmail.com>
2026-04-01 21:33:46 +0530

    handled little domain restriction bug

--- api/fetch.js
+++ api/fetch.js
@@ -1,6 +1,5 @@
 export default async function handler(req,res) {
-    // const allowedSite = 'orion-vault-two.vercel.app';
-    const allowedSite = 'localhost:3000';
+    const allowedSite = 'orion-vault-two.vercel.app';
     if(req.headers.host !== allowedSite) {
         console.log('fake url: '+req.headers.host);
         return res.status(403).json({error: 'Forbidden'})
